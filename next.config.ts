2473f0a8c037fbd5955b1992dbed1047c6527ec5
Swapnil841 <iamswapnilbamane@gmail.com>
2026-06-11 23:40:55 +0530

    fix: add accounts.google.com to CSP frame-src and script-src for Google OAuth

--- next.config.ts
+++ next.config.ts
@@ -11,12 +11,12 @@ const securityHeaders = [
     key: "Content-Security-Policy",
     value: [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://checkout.razorpay.com https://js.stripe.com https://www.googletagmanager.com https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://checkout.razorpay.com https://js.stripe.com https://www.googletagmanager.com https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ https://accounts.google.com/gsi/client",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com",
       "img-src 'self' data: blob: https://firebasestorage.googleapis.com https://images.unsplash.com https://i.ibb.co https://lh3.googleusercontent.com https://images.pexels.com",
       "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://*.firebaseapp.com https://*.render.com wss://*.firebaseio.com https://www.googleapis.com",
-      "frame-src 'self' https://checkout.razorpay.com https://js.stripe.com https://www.google.com/recaptcha/",
+      "frame-src 'self' https://accounts.google.com https://checkout.razorpay.com https://js.stripe.com https://www.google.com/recaptcha/",
     ].join("; "),
   },
 ];
